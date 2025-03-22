f5924ca5f9f0d94ad47e139e643266af4be7cc0f
Chandra Sekhar Vipparla <vipparlachandrasekhar@gmail.com>
2025-03-22 11:55:42 +0530

    commit for Queue implementations

---
+++ src/main/java/Collections_HandsOn/ArrayDequeExample.java
@@ -0,0 +1,24 @@
+package Collections_HandsOn;
+
+import java.util.ArrayDeque;
+
+public class ArrayDequeExample {
+    public static void main(String[] args) {
+        ArrayDeque<String> fruits = new ArrayDeque<>();
+        fruits.offer("apple");
+        fruits.offer("banana");
+        fruits.offer("cherry");
+        fruits.offerFirst("date");
+        while(!fruits.isEmpty()){
+            System.out.println(fruits.poll());
+        }
+        ArrayDeque<Character> letters = new ArrayDeque<>();
+        letters.push('a');
+        letters.push('b');
+        letters.push('c');
+        while(!letters.isEmpty()){
+            System.out.println(letters.pop());
+        }
+
+    }
+}
---
+++ src/main/java/Collections_HandsOn/PriorityQueueExample.java
@@ -0,0 +1,26 @@
+package Collections_HandsOn;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class PriorityQueueExample {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> numbers = new  PriorityQueue<>();
+        numbers.add(30);
+        numbers.add(10);
+        numbers.add(20);
+        numbers.add(40);
+        while(!numbers.isEmpty()){
+            System.out.println(numbers.poll());
+        }
+
+        PriorityQueue<String> names = new PriorityQueue<>(Comparator.comparing(String::length));
+        names.add("apple");
+        names.add("banana");
+        names.add("cherry");
+        names.add("date");
+        while(!names.isEmpty()){
+            System.out.println(names.poll());
+        }
+    }
+}
