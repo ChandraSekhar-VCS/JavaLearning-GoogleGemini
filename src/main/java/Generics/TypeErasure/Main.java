61e4c5a8c80da9548e3d3c83ca865161ba769da5
Chandra Sekhar Vipparla <vipparlachandrasekhar@gmail.com>
2025-03-28 18:23:54 +0530

    commit for restructuring

---
+++ src/main/java/Generics/TypeErasure/Main.java
@@ -0,0 +1,19 @@
+//package Generics.TypeErasure;
+//
+//import java.util.List;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        public static void checkIfString(List <?> list){
+//            if(list instanceof List<String>){
+//                System.out.println("This is a string list");
+//            }
+//            if(list instanceof List){
+//                System.out.println("This is a list");
+//            }
+//            else{
+//                System.out.println("This is not a list");
+//            }
+//        }
+//    }
+//}
